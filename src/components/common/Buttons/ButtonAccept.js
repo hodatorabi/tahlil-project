e69cd34d0146a8f00b66f91a47ebb9ce9619f9fd
hoda <hodat9696@gmail.com>
2019-01-09 02:15:32 +0330

    accept and reject button styles changed

--- src/components/common/Buttons/ButtonAccept.js
+++ src/components/common/Buttons/ButtonAccept.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import {ICON_ACCEPT} from 'src/assets/styles/icons'
 import {Image, TouchableOpacity} from 'react-native'
-import {COLOR_BLUE_DEFAULT} from 'src/assets/styles/colors'
+import {COLOR_BLUE_DEFAULT, COLOR_BLUE_TRANSPARENT} from 'src/assets/styles/colors'
 
 
 class ButtonAccept extends React.Component<Props, void> {
 
   render() {
     return (
-      <TouchableOpacity style={this.props.style} onPress={this.props.onPress}>
+      <TouchableOpacity
+        style={[{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: COLOR_BLUE_TRANSPARENT,
+          width: 38,
+          height: 38,
+          borderRadius: 19,
+        }, this.props.style]}
+        onPress={this.props.onPress}>
         <Image source={ICON_ACCEPT} tintColor={COLOR_BLUE_DEFAULT}
-               style={{width: 35, height: 35}}/>
+               style={{width: 22, height: 22}}/>
       </TouchableOpacity>
     )
   }
--- src/components/common/Buttons/ButtonReject.js
+++ src/components/common/Buttons/ButtonReject.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import {ICON_REJECT} from 'src/assets/styles/icons'
 import {Image, TouchableOpacity} from 'react-native'
-import {COLOR_RED} from 'src/assets/styles/colors'
+import {COLOR_RED, COLOR_RED_TRANSPARENT} from 'src/assets/styles/colors'
 
 
 class ButtonReject extends React.Component<Props, void> {
 
   render() {
     return (
-      <TouchableOpacity style={this.props.style} onPress={this.props.onPress}>
+      <TouchableOpacity style={[{
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLOR_RED_TRANSPARENT,
+        width: 38,
+        height: 38,
+        borderRadius: 19,
+      }, this.props.style]}
+                        onPress={this.props.onPress}>
         <Image source={ICON_REJECT} tintColor={COLOR_RED}
-               style={{width: 35, height: 35}}/>
+               style={{width: 20, height: 20}}/>
       </TouchableOpacity>
     )
   }
