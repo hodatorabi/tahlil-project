e90fc2b2910e74d4096bf640a949d7476f1377db
hoda <hodat9696@gmail.com>
2019-01-08 20:29:18 +0330

    outgoing request page added

--- src/components/requests/Outgoing/OutgoingRequests.js
+++ src/components/requests/Outgoing/OutgoingRequests.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {StyleSheet} from 'react-native'
+import {ScrollView, StyleSheet, View} from 'react-native'
+import {PROJECT_SAMPLE_PIC1, PROJECT_SAMPLE_PIC2, PROJECT_SAMPLE_PIC3} from 'src/assets/styles/icons'
 import OutgoingRequest from 'src/components/requests/Outgoing/OutgoingRequest'
 import {messages} from 'src/utils/messages'
 
@@ -8,7 +9,14 @@ class OutgoingRequests extends React.Component<Props, void> {
 
   render() {
     return (
-      <OutgoingRequest status={messages.ACCEPTED}/>
+      <View>
+        <ScrollView contentContainerStyle={{paddingTop: 20, alignItems: 'center'}}>
+          <OutgoingRequest projectPicture={PROJECT_SAMPLE_PIC3} status={messages.PENDING} charityName={'امام علی'}
+                           projectName={'شناسایی بی‌خانمان‌ها'}/>
+          <OutgoingRequest projectPicture={PROJECT_SAMPLE_PIC2} status={messages.ACCEPTED}/>
+          <OutgoingRequest projectPicture={PROJECT_SAMPLE_PIC1} status={messages.REJECTED}/>
+        </ScrollView>
+      </View>
     )
   }
 }
