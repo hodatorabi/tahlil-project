54d8ce4ae2434056c8a2fb1cb9cfa04c8bd51b45
hoda <hodat9696@gmail.com>
2019-02-10 18:26:48 +0330

    small changes

--- src/components/charity/home/project/CharityProjectProfile.js
+++ src/components/charity/home/project/CharityProjectProfile.js
@@ -88,14 +88,14 @@ class CharityProjectProfile extends React.Component<Props, void> {
 
           {type === messages.NON_CASH &&
           <View style={style.projectInfoContainer}>
-            <ProjectInfoRow title={messages.VOLUNTEER_AGE} description={[project.maxAge + ' تا ' + project.minAge]}/>
+            <ProjectInfoRow title={messages.VOLUNTEER_AGE} description={[project.minAge + ' تا ' + project.maxAge]}/>
             <ProjectInfoRow title={messages.VOLUNTEER_GENDER}
                             description={[booleanToGender(project.needMale, project.needFemale)]}/>
             <ProjectInfoRow title={messages.PROJECT_LOCATION} description={[project.city]}/>
             <ProjectInfoRow title={messages.VOLUNTEER_ABILITIES}
                             ability={true}
                             description={project.abilities}/>
-            <ProjectInfoRow title={messages.TIME_SCHEDULE}
+            <ProjectInfoRow title={messages.TIME_SCHEDULE + ':'}
                             timeSlot={true}
                             description={this.state.timeSlots}/>
           </View>}
